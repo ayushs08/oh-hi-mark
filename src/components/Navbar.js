449b9fddcde4b3d17c1dbcb716aeee1546775307
Ayush Sindhwani <ayushsindhwani08@gmail.com>
2018-09-14 15:57:57 +0530

    svg background

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,15 @@ import logo from '../logo.svg';
 const Navbar = () => {
   return (
     <div className="navbar">
+      <div className="copy" className="extra-item">
+        <i className="fal fa-copy"></i>
+      </div>
       <div className="logo">
         <img src={logo} alt="logo markdown preview editor"/>
       </div>
-      <div className="extra">
-        <div className="copy" className="extra-item">
-          <i className="fal fa-copy"></i>
-        </div>
-        <a href="https://github.com/ayushs08/oh-hi-mark" target="_blank" rel="noopener noreferrer" className="extra-item">
-          <i className="fal fa-code"></i>
-        </a>
-      </div>
+      <a href="https://github.com/ayushs08/oh-hi-mark" target="_blank" rel="noopener noreferrer" className="extra-item">
+        <i className="fal fa-code"></i>
+      </a>
     </div>
   )
 }
