b0becc4ead73f4d2789438e7f2e017e1f088abf6
Ayush Sindhwani <ayushsindhwani08@gmail.com>
2018-09-19 10:12:56 +0530

    marked.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,33 @@ import Navbar from './Navbar';
 import Editor from './Editor';
 import Preview from './Preview';
 
+const template = "# oh-hi-mark\n\n" +
+"React based markdown editor built as part of the FreeCodeCamp Frontend Libraries Certification program.\n\n" +
+"### Demo\n\n" +
+"http://ayushs08.github.io/oh-hi-mark\n\n" +
+"### Dependencies\n\n" +
+"* [react] - An npm package to get you immediate access to React.\n\n" +
+"* [react-dom] - This package serves as the entry point of the DOM-related rendering paths.\n\n" +
+"### Installation\n\n" +
+"Clone the repo:\n\n" +
+"```sh\n" +
+"git clone https://github.com/ayushs08/oh-hi-mark.git" +
+"```\n\n" +
+"Install the dependencies and run the server:\n\n" +
+"```sh\n" +
+"cd oh-hi-mark\n" +
+"npm install\n" +
+"npm start\n" +
+"```\n\n" +
+"### License\n\n" +
+"MIT\n\n" +
+"[react]: <https://www.npmjs.com/package/react>\n\n" +
+"[react-dom]: <https://www.npmjs.com/package/react-dom>"
+
 class App extends Component {
 
   state = {
-    markdown: ""
+    markdown: template
   }
 
   render() {
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import marked from 'marked';
 
 const Preview = ({markdown}) => {
+
+  const parseMarkdown = () => {
+    return marked(markdown)
+  }
+
   return (
     <div className="preview">
       <div className="header">preview</div>
-      <div className="area">
-        {markdown}
-      </div>
+      <div className="area" dangerouslySetInnerHTML={{__html: marked(markdown)}} ></div>
     </div>
   )
 }
